7ee479538d0ed8ef36a4968d983fcb538d71c2dc
rmanaem <rmanaem@protonmail.ch>
2021-02-10 07:21:06 -0500

    Implemented indexed draws to draw a 3D triangle (4 triangles)

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -10,7 +10,7 @@
 const GLint WIDTH = 800, HEIGHT = 600;
 const float toRadians = 3.14159265f / 180.0f;
 
-GLuint VAO, VBO, shader, uniformModel;
+GLuint VAO, VBO, IBO, shader, uniformModel;
 
 // Directions for moving the triangle
 bool direction = true;
@@ -28,13 +28,25 @@ float minSize = 0.1f;
 // Create VAO and VBO
 void createVertexArrayObject()
 {
+
+    unsigned int indices[] = {
+        0, 3, 1,
+        1, 3, 2,
+        2, 3, 0,
+        0, 1, 2};
+
     GLfloat vertices[] = {
         -1.0f, -1.0, 0.0f,
+        0.0f, -1.0f, 1.0f,
         1.0f, -1.0f, 0.0f,
         0.0f, 1.0f, 0.0f};
 
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
+
+    glGenBuffers(1, &IBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -46,6 +58,8 @@ void createVertexArrayObject()
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     glBindVertexArray(0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
 // Vertex Shader
@@ -53,10 +67,12 @@ const char *vertexShader()
 {
     return "#version 330\n"
            "layout (location = 0) in vec3 pos;"
+           "out vec4 vertexCol;"
            "uniform mat4 model;"
            "void main()"
            "{"
            "    gl_Position = model * vec4(pos, 1.0);"
+           "    vertexCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);"
            "}";
 }
 
@@ -65,9 +81,10 @@ const char *fragmentShader()
 {
     return "#version 330\n"
            "out vec4 color;"
+           "in vec4 vertexCol;"
            "void main()"
            "{"
-           "    color = vec4(1.0, 1.0, 1.0, 1.0);"
+           "    color = vertexCol;"
            "}";
 }
 
@@ -184,6 +201,8 @@ int main()
         return 1;
     }
 
+    glEnable(GL_DEPTH_TEST);
+
     // Setup Viewport size
     glViewport(0, 0, bufferWidth, bufferHeight);
 
@@ -210,7 +229,7 @@ int main()
             direction = !direction;
         }
 
-        rotationAngle += 0.001f;
+        rotationAngle += 0.0001f;
         if (rotationAngle >= 360)
         {
             rotationAngle -= 360;
@@ -232,7 +251,7 @@ int main()
 
         // Clear window
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // Drawing the triangle
         glUseProgram(shader);
@@ -240,18 +259,20 @@ int main()
         glm::mat4 model(1.0f);
 
         // Rotation
-        model = glm::rotate(model, rotationAngle, glm::vec3(0.0f, 0.0f, 1.0f));
-        // Translation
-        model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
+        model = glm::rotate(model, rotationAngle, glm::vec3(0.0f, 1.0f, 0.0f));
+        // // Translation
+        // model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
         // Scale
-        model = glm::scale(model, glm::vec3(size, size, 1.0f));
+        model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 
         // Updating the uniform variable to transform the triangle
         glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+        glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
         glUseProgram(0);
 
